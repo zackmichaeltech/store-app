6be7f1a55a007f8b94c23540b3e51300e22785c4
zackmichaeltech <zackmichaeltech@gmail.com>
2023-07-24 20:57:52 +0200

    changes in views.py and path.py, getting redirected to a singular product page
    Creating the singular product page; creating  appropriate function product_info and its url   path('product/<slug:slug>/') in urls.py

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from . models import Category, Product
 
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 
 def store (request):
@@ -17,6 +19,13 @@ def categories(request):
 
     return {'all_categories': all_categories}
 
+def product_info(request,slug):
+
+    product = get_object_or_404(Product,slug=slug) #getting hold of a specific product from a database,
+                                                    #if it doesn't exist return an error, match the product with
+    context = {'product':product}                   #a slug ID, that is equal to a slug ID we are looking for
+
+    return render(request,'store/product-info.html',context)
 
 
 
