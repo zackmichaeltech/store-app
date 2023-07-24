016c28de41e3b0b40c39d9f0169f718b43dd3e38
zackmichaeltech <zackmichaeltech@gmail.com>
2023-07-24 18:04:05 +0200

    prepopulated fields in admin.py
    adding prepopulated fields in admin.py

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -3,9 +3,18 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Category,Product    #registering the models
+#prepopulated with name and title
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
 
-admin.site.register(Category)
+    prepopulated_fields={'slug':('name',)}#added automatically and sharing the name value
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug':('title')}
+
+
+
+
 
 
