f75c481c57174be2893a6a3ad56ee6768c403148
zackmichaeltech <zackmichaeltech@gmail.com>
2023-07-27 20:04:50 +0200

    Testing session in JSON
    checking whether a session is properly in place with JSON request in cart views.py; referencing product. title in a JSON response (also added proper JSON reference in product-info.html); each response unique for each product

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -5,6 +5,8 @@ from.cart import Cart #importing cart class from the Cart
 from store.models import Product
 from django.shortcuts import get_object_or_404
 
+from django.http import JsonResponse
+
 # Create your views here.
 
 
@@ -13,11 +15,11 @@ def cart_summary(request):
     return render(request,'cart/cart-summary.html')
 
 
-def cart_add(request):   #grabbing AJAX functionality from product-info
+def cart_add(request):                           #grabbing AJAX functionality from product-info
 
-    cart=Cart(request) #using session data
+    cart=Cart(request)                            #using session data
 
-    if request.POST.get('action') =='POST': #verifying AJAX request, if correct accessing it
+    if request.POST.get('action') =='post':       #verifying AJAX request, if correct accessing it
 
         product_id= int(request.POST.get('product_id'))
         product_quantity = int(request.POST.get('product_quantity'))
@@ -26,11 +28,11 @@ def cart_add(request):   #grabbing AJAX functionality from product-info
 
         product = get_object_or_404(Product, id=product_id)
 
-        cart.add(product=product, product_qty=product_quantity) #getting a particular product along with its quantity
+        cart.add(product=product, product_qty=product_quantity)      #getting a particular product along with its quantity
 
+        response = JsonResponse({'The product is called:': product.title, 'and the product quantity is:':product_quantity})
 
-
-
+        return response
 
 def cart_delete(request):
 
