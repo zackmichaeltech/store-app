461e98c26d8133253d55be8779358c1e3231b852
zackmichaeltech <zackmichaeltech@gmail.com>
2023-07-27 19:43:44 +0200

    functionality of adding a product into a cart
    Creation of a function responsible for adding a product to a cart (using product-info.html) inside cart.views.py, afterwards creation of a class in carts.py responsible for the handling of a session adding a product into a cart

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -6,12 +6,25 @@ class Cart():
         self.session = request.session  #creating new session for new user, returning one utilize existing session
         cart=self.session.get('session_key') #for existing user
 
-    # new key for new user
-
+        # new key for new user
         if 'session_key' not in request.session:
 
             cart=self.session['session_key'] = {}
 
-    # new user no products in cart
-        self.cart=cart
+        self.cart=cart         # new user no products in cart
 
+
+
+    def add(self,product,product_qty):        #passing add function into an add class
+
+        product_id = str(product.id)     # str so only the quantity can get modified
+
+        if product_id in self.cart:         #if product is in the cart
+
+            self.cart[product_id]['qty'] = product_qty     #modifying the quantity if necessary
+
+        else: #if the product not in the cart, taking the price and qty
+
+            self.cart[product_id] = {'price':str (product.price),'qty':product_qty}
+
+        self.session.modified = True #the session has been modified
--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from.cart import Cart #importing cart class from the Cart
+
+from store.models import Product
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 
 
@@ -8,9 +13,23 @@ def cart_summary(request):
     return render(request,'cart/cart-summary.html')
 
 
-def cart_add(request):
+def cart_add(request):   #grabbing AJAX functionality from product-info
 
-    pass
+    cart=Cart(request) #using session data
+
+    if request.POST.get('action') =='POST': #verifying AJAX request, if correct accessing it
+
+        product_id= int(request.POST.get('product_id'))
+        product_quantity = int(request.POST.get('product_quantity'))
+
+        #performing a query on a Product class object that is equal to AJAX request
+
+        product = get_object_or_404(Product, id=product_id)
+
+        cart.add(product=product, product_qty=product_quantity) #getting a particular product along with its quantity
+
+
+
 
 
 def cart_delete(request):
