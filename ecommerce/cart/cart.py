0291cce2d2cb6ac1147bc4034a3ba1a3488c3f4c
zackmichaeltech <zackmichaeltech@gmail.com>
2023-07-28 13:26:23 +0200

    Adding delete from cart functionality
    .DELETE integration with AJAX; cart-summary delete; passing the product.id into a delete button; adding AJAX script responsible for delete function at the bottom of cart-summary.html
    
    DELETE functionality, working on cart_delete in cart views.py, also creating a def delete function in cart.py; adding the keys referenced in json response in views.py to cart-summary.html

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -31,6 +31,15 @@ class Cart():
 
         self.session.modified = True                                                  #the session has been modified
 
+    def delete(self,product):
+
+        product_id=str(product)
+
+        if product_id in self.cart:
+
+            del self.cart[product_id]
+
+        self.session.modified = True
 
 
 
--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 
-from.cart import Cart #importing cart class from the Cart
+from.cart import Cart                                                                                    #importing cart class from the Cart
 
 from store.models import Product
 from django.shortcuts import get_object_or_404
 
 from django.http import JsonResponse
 
-# Create your views here.
 
 
 def cart_summary(request):
@@ -17,11 +16,11 @@ def cart_summary(request):
     return render(request,'cart/cart-summary.html',{'cart':cart})
 
 
-def cart_add(request):                           #grabbing AJAX functionality from product-info
+def cart_add(request):                                                                                    #grabbing AJAX functionality from product-info
 
-    cart=Cart(request)                            #using session data
+    cart=Cart(request)                                                                                    #using session data
 
-    if request.POST.get('action') =='post':       #verifying AJAX request, if correct accessing it
+    if request.POST.get('action') =='post':                                                              #verifying AJAX request, if correct accessing it
 
         product_id= int(request.POST.get('product_id'))
         product_quantity = int(request.POST.get('product_quantity'))
@@ -30,10 +29,10 @@ def cart_add(request):                           #grabbing AJAX functionality fr
 
         product = get_object_or_404(Product, id=product_id)
 
-        cart.add(product=product, product_qty=product_quantity)      #getting a particular product along with its quantity
+        cart.add(product=product, product_qty=product_quantity)                                           #getting a particular product along with its quantity
 
 
-        cart_quantity = cart.__len__()                               #total quantity from session data
+        cart_quantity = cart.__len__()                                                                     #total quantity from session data
 
 
         response = JsonResponse({'qty':cart_quantity})
@@ -42,8 +41,21 @@ def cart_add(request):                           #grabbing AJAX functionality fr
 
 def cart_delete(request):
 
-    pass
+    cart=Cart(request)
 
+    if request.POST.get('action') =='post':
+
+        product_id= int(request.POST.get('product_id'))
+
+        cart.delete(product=product_id)                                                                      #data sent from frontend to backend
+
+        cart_quantity = cart.__len__()                                                                       #the quantity updated based on a current session
+
+        cart_total= cart.get_total()
+
+        response = JsonResponse({'qty':cart_quantity,'total':cart_total})
+
+        return response
 
 def cart_update(request):
 
