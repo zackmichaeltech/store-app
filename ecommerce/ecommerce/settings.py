f3e03a3add73dfc812db4c2042b5a6d4d882be15
zackmichaeltech <zackmichaeltech@gmail.com>
2023-07-29 22:33:39 +0200

    EMAIL_BACKEND in settings.py
    commenting out EMAIL_BACKEND in order to test email sending in the console, adding the relevant parts for setting up email sending in GOOGLE

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -145,4 +145,15 @@ MEDIA_ROOT=BASE_DIR/'static/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
+
+
+#EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
+
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT='587'
+EMAIL_USE_TLS='True'
+
+EMAIL_HOST_USER='' #Your gmail address
+EMAIL_HOST_PASSWORD=''  #Your app password
