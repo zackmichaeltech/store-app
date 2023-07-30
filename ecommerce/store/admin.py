f34810e00c68a0337fd848dfc6d0ac945e83fdec
zackmichaeltech <zackmichaeltech@gmail.com>
2023-07-30 23:13:17 +0200

    Additional many-to-many model in store views.py
    Adding additional many to many model Tag in store.models.py allowing multiple tags to be associated with multiple products

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,17 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
 
-from .models import Category,Product    #registering the models
+from .models import Category,Product,Tag                                            #registering the models
 #prepopulated with name and title
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
 
-    prepopulated_fields={'slug':('name',)}#added automatically and sharing the name value
+    prepopulated_fields={'slug':('name',)}                     #added automatically and sharing the name value
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields={'slug':('title',)}
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields={'tag_slug':('tag_name',)}
 
 
 
--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -34,20 +34,23 @@ class Product(models.Model):
 
     brand=models.CharField(max_length=250,default='un-branded')
 
-    description= models.TextField(blank=True)                                               #blank denotes an optional field
+    description= models.TextField(blank=True)                                                  #blank denotes an optional field
 
     slug=models.SlugField(max_length=255)                                                       #to make urls more user-friendly and readable and unique
 
-    price=models.DecimalField(max_digits=5,decimal_places=2)                                                            #product price
+    price=models.DecimalField(max_digits=5,decimal_places=2)                                   #product price
 
     image = models.ImageField(upload_to='images/')                                #pillow installation required; upon uploading an image will create an image subfolder in media
 
+    tags = models.ManyToManyField('Tag', related_name='product_tags', blank=True)                   #MANY TO MANY RELATIONSHIP with tags
+
+
     class Meta:
-        verbose_name_plural = 'products'                                                    #django by default adds an s to category name
+        verbose_name_plural = 'products'                                                       #django by default adds an s to category name
 
 
     def __str__(self):
-        return self.title  #(products referenced by their correct title instead of product1, or product2)
+        return self.title                                                                        #(products referenced by their correct title instead of product1, or product2)
 
     # 3. makemigrations, migrate
 
@@ -55,3 +58,11 @@ class Product(models.Model):
     def get_absolute_url(self):
 
         return reverse('product-info',args=[self.slug])
+
+
+class Tag(models.Model):                                                                                                # Many to many relationship with Products category
+    tag_name = models.CharField(max_length=128)
+    tag_slug = models.SlugField(max_length=255)                                                                         # BEST SELLERS, NEW ARRIVALS
+
+    def __str__(self):
+        return self.tag_name
